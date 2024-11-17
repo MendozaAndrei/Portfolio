9212591c1eb18a8313219467034f08f76bfd3418
Padrei_Juan <amendoza39@gmail.com>
2024-11-17 15:54:21 -0800

    Removed secrets from app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var passport = require('passport');
 var session = require('express-session');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
-  clientID: '393776785648-o2vb0055ana0f1ef5ukr5u0surhqa5l7.apps.googleusercontent.com', //you get this from Google, refer to your Part II
-  clientSecret: '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', //you get this from Google, refer to your Part II
+  clientID: process.env.GOOGLE_CLIENT_ID, //you get this from Google, refer to your Part II
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET, //you get this from Google, refer to your Part II
   callbackURL: 'https://portfolio.andrei.lol/auth/google/callback'},
   function(accessToken, refreshToken, profile, cb) {
   cb(null, profile);
