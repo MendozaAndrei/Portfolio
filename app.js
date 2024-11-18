a8c974943ed397033cedbc2e1e58888b029a4bd8
Padrei_Juan <amendoza39@gmail.com>
2024-11-17 16:16:35 -0800

    some changes

--- app.js
+++ app.js
@@ -3,24 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var auth = require ("./routes/auth")
+var auth = require('./routes/auth');
+
 var app = express();
-var passport = require('passport');
-var session = require('express-session');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Configure session middleware
+app.use(session({
+  secret: 'your_secret_key', // Replace with a strong secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID, //you get this from Google, refer to your Part II
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET, //you get this from Google, refer to your Part II
-  callbackURL: 'https://portfolio.andrei.lol/auth/google/callback'},
-  function(accessToken, refreshToken, profile, cb) {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://portfolio.andrei.lol/auth/google/callback'
+}, function(accessToken, refreshToken, profile, cb) {
   cb(null, profile);
-  }
-));
+}));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,30 +53,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/auth', auth)
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-app.use(session({secret: 'cat'}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
+app.use('/auth', auth);
 
 module.exports = app;
